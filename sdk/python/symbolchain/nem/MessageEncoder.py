a1cf2fe65edbf3a5abdcf4c495218cdc477b6211
gimre <g.imre@protonmail.com>
2022-07-28 15:15:40 +0200

    [sdk/python]: extract shared pieces to cipher helpers

--- sdk/python/symbolchain/nem/MessageEncoder.py
+++ sdk/python/symbolchain/nem/MessageEncoder.py
@@ -1,9 +1,7 @@
-import secrets
-
 import cryptography
 
-from symbolchain.Cipher import AesCbcCipher, AesGcmCipher
 from symbolchain.CryptoTypes import PublicKey
+from symbolchain.impl.CipherHelpers import decode_aes_cbc, decode_aes_gcm, encode_aes_cbc, encode_aes_gcm
 from symbolchain.nc import Message, MessageType
 from symbolchain.nem.KeyPair import KeyPair
 from symbolchain.nem.SharedKey import SharedKey
@@ -12,31 +10,9 @@ from symbolchain.nem.SharedKey import SharedKey
 class MessageEncoder:
 	"""Encrypts and encodes messages between two parties."""
 
-	GCM_IV_SIZE = 12
-	CBC_IV_SIZE = 16
-	SALT_SIZE = 32
-
 	def __init__(self, key_pair: KeyPair):
 		"""Creates message encoder around key pair."""
 		self.key_pair = key_pair
-
-	def _decode_aes_gcm(self, recipient_public_key, encoded_message):
-		# pylint: disable=duplicate-code
-		tag = encoded_message[:AesGcmCipher.TAG_SIZE]
-		initialization_vector = encoded_message[AesGcmCipher.TAG_SIZE:AesGcmCipher.TAG_SIZE + self.GCM_IV_SIZE]
-		encoded_message_data = encoded_message[AesGcmCipher.TAG_SIZE + self.GCM_IV_SIZE:]
-
-		shared_key = SharedKey.derive_shared_key(self.key_pair, recipient_public_key)
-		cipher = AesGcmCipher(shared_key)
-		return cipher.decrypt(encoded_message_data + tag, initialization_vector)
-
-	def _decode_aes_cbc(self, recipient_public_key, encoded_message):
-		salt = encoded_message[:self.SALT_SIZE]
-		initialization_vector = encoded_message[self.SALT_SIZE:self.SALT_SIZE + self.CBC_IV_SIZE]
-		encoded_message_data = encoded_message[self.SALT_SIZE + self.CBC_IV_SIZE:]
-		shared_key = SharedKey.derive_shared_key_deprecated(self.key_pair, recipient_public_key, salt)
-		cipher = AesCbcCipher(shared_key)
-		return cipher.decrypt(encoded_message_data, initialization_vector)
 
 	def try_decode(self, recipient_public_key, encoded_message: Message):
 		"""Tries to decode encoded message, returns tuple:
@@ -48,13 +24,13 @@ class MessageEncoder:
 			raise RuntimeError('invalid message format')
 
 		try:
-			message = self._decode_aes_gcm(recipient_public_key, encoded_message.message)
+			message = decode_aes_gcm(SharedKey, self.key_pair, recipient_public_key, encoded_message.message)
 			return True, message
 		except cryptography.exceptions.InvalidTag:
 			pass
 
 		try:
-			message = self._decode_aes_cbc(recipient_public_key, encoded_message.message)
+			message = decode_aes_cbc(SharedKey, self.key_pair, recipient_public_key, encoded_message.message)
 			return True, message
 		except ValueError as exception:
 			if 'Invalid padding bytes' not in str(exception):
@@ -65,12 +41,7 @@ class MessageEncoder:
 	def encode_deprecated(self, recipient_public_key: PublicKey, message: bytes):
 		"""Encodes message to recipient using deprecated encryption and key derivation."""
 
-		salt = secrets.token_bytes(self.SALT_SIZE)
-		shared_key = SharedKey.derive_shared_key_deprecated(self.key_pair, recipient_public_key, salt)
-		cipher = AesCbcCipher(shared_key)
-
-		initialization_vector = secrets.token_bytes(self.CBC_IV_SIZE)
-		cipher_text = cipher.encrypt(message, initialization_vector)
+		salt, initialization_vector, cipher_text = encode_aes_cbc(SharedKey, self.key_pair, recipient_public_key, message)
 
 		encoded_messsage = Message()
 		encoded_messsage.message_type = MessageType.ENCRYPTED
@@ -79,18 +50,10 @@ class MessageEncoder:
 
 	def encode(self, recipient_public_key: PublicKey, message: bytes):
 		"""Encodes message to recipient using recommended format."""
-		# pylint: disable=duplicate-code
 
-		shared_key = SharedKey.derive_shared_key(self.key_pair, recipient_public_key)
-		cipher = AesGcmCipher(shared_key)
-
-		initialization_vector = secrets.token_bytes(self.GCM_IV_SIZE)
-		cipher_text = cipher.encrypt(message, initialization_vector)
-
-		tag_start_offset = len(cipher_text) - AesGcmCipher.TAG_SIZE
-		tag = cipher_text[tag_start_offset:]
+		tag, initialization_vector, cipher_text = encode_aes_gcm(SharedKey, self.key_pair, recipient_public_key, message)
 
 		encoded_messsage = Message()
 		encoded_messsage.message_type = MessageType.ENCRYPTED
-		encoded_messsage.message = tag + initialization_vector + cipher_text[:tag_start_offset]
+		encoded_messsage.message = tag + initialization_vector + cipher_text
 		return encoded_messsage
